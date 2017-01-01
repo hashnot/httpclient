d1c6ac6c01410636dd878c2d693a1816513525ba
Rafał Krupiński <rkrupinski+github@gmail.com>
2017-01-01 14:25:08 +0000

    Move configuration related stuff to config.go

--- httpclient/config.go
+++ httpclient/config.go
@@ -1,8 +1,97 @@
 package httpclient
 
-import "github.com/hashnot/function"
+import (
+	"github.com/hashnot/function"
+	"github.com/hashnot/function/amqptypes"
+	"net/url"
+	"github.com/rafalkrupinski/rev-api-gw/httplog"
+	"bytes"
+	"text/template"
+	"net/http"
+	"errors"
+)
+
+type HttpClient struct {
+	Function *amqptypes.Configuration `yaml:"function"`
+	Tasks    map[string]*HttpTask     `yaml:"tasks"`
+
+	verbose  bool
+}
+
+func (client *HttpClient) Setup(verbose bool) error {
+	for name, task := range client.Tasks {
+		err := task.setup(name, verbose)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 type OutputConfig struct {
 	OmitEmpty bool `yaml:"omitEmpty"`
 	function.Message
+}
+
+type HttpTask struct {
+	Source HttpInputSpec `yaml:"source"`
+	Output OutputConfig  `yaml:"output"`
+}
+
+func (task *HttpTask) setup(name string, verbose bool) error {
+	err := task.Source.parseTemplates(name)
+	if err != nil {
+		return err
+	}
+
+	err = task.Source.setupTransport(verbose)
+	if err != nil {
+		return errors.New(name + ": " + err.Error())
+	}
+	return nil
+}
+
+type HttpInputSpec struct {
+	Method       string `yaml:"method"`
+	Address      string `yaml:"address"`
+	Proxy        string `yaml:"proxy"`
+
+	client       *http.Client
+	addressTempl *template.Template
+}
+
+func (spec *HttpInputSpec) parseTemplates(name string) error {
+	spec.addressTempl = template.New(name + ".address")
+	_, err := spec.addressTempl.Parse(spec.Address)
+	return err
+}
+
+func (spec *HttpInputSpec) setupTransport(verbose bool) error {
+	proxyAddr := spec.Proxy
+
+	var transport http.RoundTripper
+
+	if proxyAddr == "" {
+		transport = http.DefaultTransport
+	} else {
+		proxyUrl, err := url.Parse(proxyAddr)
+		if err != nil {
+			return nil
+		}
+		transport = &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
+	}
+
+	if verbose {
+		transport = &httplog.LoggingRoundTripper{transport}
+	}
+
+	httpClient := &http.Client{Transport: transport}
+	spec.client = httpClient
+	return nil
+}
+
+func apply(t *template.Template, data *function.Message) (string, error) {
+	buffer := new(bytes.Buffer)
+	err := t.Execute(buffer, data)
+	return buffer.String(), err
 }
--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -4,22 +4,11 @@ import (
 	"bytes"
 	"errors"
 	"github.com/hashnot/function"
-	"github.com/hashnot/function/amqptypes"
-	"github.com/rafalkrupinski/rev-api-gw/httplog"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
-	"text/template"
 	"reflect"
 )
-
-type HttpClient struct {
-	Function *amqptypes.Configuration `yaml:"function"`
-	Tasks    map[string]*HttpTask     `yaml:"tasks"`
-
-	verbose  bool
-}
 
 //Input:
 //http body from payload
@@ -96,77 +85,4 @@ func (task *HttpTask)responseToMessage(resp *http.Response) (*function.Message, 
 	result.ContentType = resp.Header.Get("Content-Type")
 
 	return &result, nil
-}
-
-func (client *HttpClient) Setup(verbose bool) error {
-	for name, task := range client.Tasks {
-		err := task.setup(name, verbose)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-type HttpTask struct {
-	Source HttpInputSpec `yaml:"source"`
-	Output OutputConfig  `yaml:"output"`
-}
-
-func (task *HttpTask) setup(name string, verbose bool) error {
-	err := task.Source.parseTemplates(name)
-	if err != nil {
-		return err
-	}
-
-	err = task.Source.setupTransport(verbose)
-	if err != nil {
-		return errors.New(name + ": " + err.Error())
-	}
-	return nil
-}
-
-type HttpInputSpec struct {
-	Method       string `yaml:"method"`
-	Address      string `yaml:"address"`
-	Proxy        string `yaml:"proxy"`
-
-	client       *http.Client
-	addressTempl *template.Template
-}
-
-func (spec *HttpInputSpec) parseTemplates(name string) error {
-	spec.addressTempl = template.New(name + ".address")
-	_, err := spec.addressTempl.Parse(spec.Address)
-	return err
-}
-
-func (spec *HttpInputSpec) setupTransport(verbose bool) error {
-	proxyAddr := spec.Proxy
-
-	var transport http.RoundTripper
-
-	if proxyAddr == "" {
-		transport = http.DefaultTransport
-	} else {
-		proxyUrl, err := url.Parse(proxyAddr)
-		if err != nil {
-			return nil
-		}
-		transport = &http.Transport{Proxy: http.ProxyURL(proxyUrl)}
-	}
-
-	if verbose {
-		transport = &httplog.LoggingRoundTripper{transport}
-	}
-
-	httpClient := &http.Client{Transport: transport}
-	spec.client = httpClient
-	return nil
-}
-
-func apply(t *template.Template, data *function.Message) (string, error) {
-	buffer := new(bytes.Buffer)
-	err := t.Execute(buffer, data)
-	return buffer.String(), err
 }
