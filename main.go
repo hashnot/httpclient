9846a56e0cfd303b7684a92c510351c04f4164bd
Rafał Krupiński <rkrupinski+github@gmail.com>
2017-01-07 17:19:26 +0000

    Update import for repository change

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"bitbucket.org/hashnot/httpclient/httpclient"
 	"flag"
 	"github.com/hashnot/function"
+	"github.com/hashnot/httpclient/httpclient"
 	"log"
 	"os"
 	"os/signal"
